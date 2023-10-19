09e068df34b30a59471036f52e5cfc448273baa5
masanocki <sanockimaciej00@gmail.com>
2023-10-19 17:18:09 +0200

    B)

---
+++ src/Entity/City.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CityRepository::class)]
+class City
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 2)]
+    private ?string $country = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 7)]
+    private ?string $latitude = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 7)]
+    private ?string $longitude = null;
+
+    #[ORM\OneToMany(mappedBy: 'city', targetEntity: Forecast::class)]
+    private Collection $forecasts;
+
+    public function __construct()
+    {
+        $this->forecasts = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?string
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(string $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?string
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(string $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Forecast>
+     */
+    public function getForecasts(): Collection
+    {
+        return $this->forecasts;
+    }
+
+    public function addForecast(Forecast $forecast): static
+    {
+        if (!$this->forecasts->contains($forecast)) {
+            $this->forecasts->add($forecast);
+            $forecast->setCity($this);
+        }
+
+        return $this;
+    }
+
+    public function removeForecast(Forecast $forecast): static
+    {
+        if ($this->forecasts->removeElement($forecast)) {
+            // set the owning side to null (unless already changed)
+            if ($forecast->getCity() === $this) {
+                $forecast->setCity(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Forecast.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ForecastRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ForecastRepository::class)]
+class Forecast
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'forecasts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?City $city = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: '0')]
+    private ?string $temperature = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $humidity = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $weathercondition = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $windspeed = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCity(): ?City
+    {
+        return $this->city;
+    }
+
+    public function setCity(?City $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getTemperature(): ?string
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(string $temperature): static
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getHumidity(): ?string
+    {
+        return $this->humidity;
+    }
+
+    public function setHumidity(string $humidity): static
+    {
+        $this->humidity = $humidity;
+
+        return $this;
+    }
+
+    public function getWeathercondition(): ?string
+    {
+        return $this->weathercondition;
+    }
+
+    public function setWeathercondition(string $weathercondition): static
+    {
+        $this->weathercondition = $weathercondition;
+
+        return $this;
+    }
+
+    public function getWindspeed(): ?string
+    {
+        return $this->windspeed;
+    }
+
+    public function setWindspeed(string $windspeed): static
+    {
+        $this->windspeed = $windspeed;
+
+        return $this;
+    }
+}
